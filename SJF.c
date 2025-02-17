b925d97b370a63a45f575438e7b84f7be0af7ea0
Karri Rajeswara Reddy <153280024+K-R-Reddy@users.noreply.github.com>
2025-02-17 12:01:12 +0530

    OS

---
+++ SJF.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+        int pid[15],bt[15],n,ct[15],wt[15],s=0,temp;
+        float avg;
+        printf("Enter Number of Process :: ");
+        scanf("%d",&n);
+        printf("Enter %d Process Ids :: \n",n);
+        for (int i = 0; i < n; i++)
+        {
+        scanf("%d",&pid[i]);
+        }
+        for(int i=0; i < n;i++){
+                printf("Enter process %d Burst Time :: ",pid[i]);
+                scanf("%d",&bt[i]);
+        }
+        for(int i=0;i<n-1;i++){
+                for(int j=i+1;j<n;j++){
+                        if(bt[i]>bt[j]){
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                temp=pid[i];
+                pid[i]=pid[j];
+                pid[j]=temp;
+                        }
+                }
+
+        }
+        ct[0]=0;
+                for(int i=0;i<n;i++){
+                        ct[i+1]=ct[i]+bt[i];
+                        wt[i]=ct[i+1]-bt[i];
+                        s=s+wt[i];
+                }
+        printf("|        Process Id       |       Burst Time       |       Complete Time      |       Waiting Time      |\n");
+        printf("|-------------------------|------------------------|--------------------------|-------------------------|\n");
+        for(int i=0;i<n;i++){
+                printf("|%25d|%24d|%26d|%25d|\n",pid[i],bt[i],ct[i+1],wt[i]);
+        }
+        avg=(float)s/n;
+        printf("Average Waiting Time using First Come First Serve Technique :: %.2fms",avg);
+        printf("\n"); 
+        return 0;
+}
