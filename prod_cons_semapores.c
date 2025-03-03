87b7a4f5ad7c0f7ea384d164bff41c69c643e173
Karri Rajeswara Reddy <153280024+K-R-Reddy@users.noreply.github.com>
2025-03-03 10:42:50 +0530

    OS Lab

---
+++ prod_cons_semapores.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int mutex=1,fill=0,empty=10;
+void producer(){
+        while(mutex==0);
+        mutex--;
+        if(empty==0){
+        printf("Buffer is Full");
+        mutex++;
+        }
+        else{
+        fill++;
+        empty--;
+        printf("Producer Produces 1 good.Therefore Producer contains %d goods",fill);
+        mutex--;    
+        }
+}
+void consumer(){
+        while(mutex==0);
+        mutex--;
+        if(fill==0){
+        printf("Buffer is Empty");
+        mutex++;
+        }
+        else{
+        fill--;
+        empty++;
+        printf("Consumer consumes 1 good.Therefore Producer contains %d goods",fill);
+        mutex--;    
+        }
+}
+int main(){
+        int ch;
+        printf("Enter 1 for producer produces good\nEnter 2 for Consumer to consume good\nEnter 3 to Exit\n");
+        do{
+                printf("\nEnter your Choice ::");
+                scanf("%d",&ch);
+                switch (ch)
+                {
+                case 1:
+                        producer();
+                        break;
+                case 2:
+                        consumer();
+                        break;
+                default:
+                        printf("Program Terminated\n");
+                        break;
+                }
+        }while(ch!=3);
+        return 0;
+}
